10ded68544d462852a11b6e7b5dc65465b89f995
Tudor Hulban <tudi@J3710>
2020-03-25 11:32:40 +0200

    added logging for logs during lifecycle

--- goversion.go
+++ goversion.go
@@ -11,9 +11,12 @@ var version string
 
 func main() {
 	log.Println("Binary verions: " + version)
+
 	handler := func(w http.ResponseWriter, req *http.Request) {
+		log.Println("Request Header: ", req.Header) // added for checking logs during app lifecycle
 		io.WriteString(w, "Version: "+version)
 	}
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
